4be8d3118e7018555fe18c0f9a0769c0bde60553
ketanspage <gandhi.ketan55555@gmail.com>
2023-09-11 20:13:15 +0530

    update

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -18,6 +18,7 @@ function AddTodo() {
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-gray-100 py-2 px-3 outline-none transition-colors duration-200 ease-in-out"
+        
         placeholder="Enter a Todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
